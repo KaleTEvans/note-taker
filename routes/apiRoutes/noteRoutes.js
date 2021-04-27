f568bb593028073c0083003775575d4e4fb279f8
KaleTEvans <kaletevans@gmail.com>
2021-04-27 16:10:49 -0500

    added a delete function

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -18,6 +18,16 @@ router.get('/notes/:id', (req, res) => {
     }
 });
 
+// delete selected note
+router.delete('/notes/:id', (req, res) => {
+    let result = findById(req.params.id, notes);
+    if (result) {
+        notes.splice(result, 1);
+    } else {
+        res.send(404);
+    }
+});
+
 // post a new note
 router.post('/notes', (req, res) => {
     // give note a new id
