f0ea2d96dc1f26d5db95224c10e49157e7c67ef1
KaleTEvans <kaletevans@gmail.com>
2021-04-27 15:15:07 -0500

    created paths to save and view notes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,9 +1,34 @@
 const router = require('express').Router();
 
+const { createNewNote, validateNote, findById } = require('../../lib/notes');
 const { notes } = require('../../db/notes');
 
 router.get('/notes', (req, res) => {
-    res.json(notes);
+    let results = notes;
+    res.json(results);
+});
+
+// find note by id when clicked on 
+router.get('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+// post a new note
+router.post('/notes', (req, res) => {
+    // give note a new id
+    req.body.id = notes.length.toString();
+
+    if (!validateNote(req.body)) {
+        req.statusCode(400).send('This note is not properly formatted');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
 });
 
 module.exports = router;
